bb4c414dbcd9a7768c071cea4dea1bae7d7ad2cd
Andrew <andrew.alan.sm@gmail.com>
2025-02-19 18:02:43 +0000

    set bullet style for list (custom bullet style without the trailing period)

--- src/views/Rules.jsx
+++ src/views/Rules.jsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router';
+import styles from './rules.module.css';
+import tickButton from '../assets/images/tick.svg';
+
 export default function Rules() {
     return (
-        <h1>Rules</h1>
+        <div className={styles.viewContainer}>
+            <div className={styles.contentContainer}>
+                <h1>Rules</h1>
+                <h2>Objective</h2>
+                <p>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).</p>
+                <h2>How To Play</h2>
+                <ol>
+                    <li>Red goes first in the first game.</li>
+                    <li>Players must alternate turns, and only one disc can be dropped in each turn.</li>
+                    <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
+                    <li>The starter of the previous game goes second on the next game.</li>
+                </ol>
+                <Link to='/' >
+                    {/* TODO: Add text version of button for accessibility */}
+                    <img src={tickButton} alt='return to menu' />
+                </Link>
+            </div>
+        </div>
+        
     )
 }
