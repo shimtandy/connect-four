c41ffae54edd61b5a38fb47bd53e71f3e181a43d
Andrew <andrew.alan.sm@gmail.com>
2025-02-23 22:27:14 +0000

    add entrance transition for disks using 'motion' library

--- src/components/Disk.jsx
+++ src/components/Disk.jsx
@@ -1,19 +1,40 @@
 import { useEffect, useRef } from 'react'
+import { motion } from 'motion/react'
 import styles from './disk.module.css'
 
+
+// From https://easings.net/#easeOutBounce under GNU GPL3 license.
+function bounceEase(x) {
+    const n1 = 7.5625
+    const d1 = 2.75
+
+    if (x < 1 / d1) {
+        return n1 * x * x
+    } else if (x < 2 / d1) {
+        return n1 * (x -= 1.5 / d1) * x + 0.75
+    } else if (x < 2.5 / d1) {
+        return n1 * (x -= 2.25 / d1) * x + 0.9375
+    } else {
+        return n1 * (x -= 2.625 / d1) * x + 0.984375
+    }
+}
+
 export default function Disk({gridX, gridY, cellSize=50, player}) {
-    const elementRef = useRef(null)
-
-    useEffect(() => {
-        elementRef.current.style.top = gridY * cellSize + 'px'
-        elementRef.current.style.transition = 'top 1s linear'
-    }, [])
-
     return (
-        <div 
+        <motion.div 
             className={styles.disk + ' ' + (player == 1 ? styles.player1 : styles.player2)}
-            ref={elementRef} 
-            style={{left: cellSize * gridX + 'px'}}>
-        </div>
+            initial={{
+                top: 0
+            }}
+            animate={{
+                top: gridY * cellSize + 'px',
+            }}
+            transition={{
+                duration: 1,
+                ease: bounceEase
+            }}
+            style={{
+                left: gridX * cellSize
+            }} />
     )
 }
