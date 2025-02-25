b0979609167bb404927b8f309cc0cdb9cd84f6fa
Andrew <andrew.alan.sm@gmail.com>
2025-02-25 11:17:26 +0000

    refactor PlayerIndicator

---
+++ src/components/PlayerIndicator.jsx
@@ -0,0 +1,48 @@
+import styles from './PlayerIndicator.module.css'
+import { useEffect, useState } from 'react'
+
+export default function PlayerIndicator({currentTurn, setCurrentTurn}) {
+
+    let [timeLeft, setTimeLeft] = useState(10)
+
+    useEffect(() => {
+        const interval = setInterval(() => setTimeLeft(timeLeft - 1), 1000)
+
+        if (timeLeft === 0) {
+            setCurrentTurn(currentTurn === 1 ? 2 : 1)
+            setTimeLeft(10)
+        }
+
+        return () => clearInterval(interval)
+    }, [timeLeft])
+
+    const indicatorSVG = 
+    <svg className={styles.indicatorImage} width="197" height="165" viewBox="0 0 197 165" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_5_3228)">
+            <path fillRule="evenodd" clipRule="evenodd" d="M3 55.2795C3 47.2326 7.82258 39.9694 15.2389 36.8468L90.2793 5.25082C95.2186 3.17114 100.786 3.16075 105.733 5.22198L181.692 36.8718C189.145 39.9772 194 47.2593 194 55.3333V132C194 143.046 185.046 152 174 152H23C11.9543 152 3 143.046 3 132V55.2795Z" fill="#FD6687" />
+            <path d="M14.6568 35.4643C6.68427 38.8212 1.5 46.6291 1.5 55.2795V132C1.5 143.874 11.1259 153.5 23 153.5H174C185.874 153.5 195.5 143.874 195.5 132V55.3333C195.5 46.6538 190.281 38.8255 182.269 35.4872L106.31 3.83737C100.992 1.62154 95.0069 1.63271 89.6972 3.86836L14.6568 35.4643Z" stroke="black" strokeWidth="3" />
+        </g>
+        <defs>
+            <filter id="filter0_d_5_3228" x="0" y="0.683594" width="197" height="164.316" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                <feOffset dy="10" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0" />
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_5_3228" />
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_5_3228" result="shape" />
+            </filter>
+        </defs>
+    </svg>
+
+    return (
+        <div className={styles.indicatorSection}>
+                        <div className={styles.indicator}>
+                            {indicatorSVG}
+                            <div className={styles.indicatorText}>
+                                <div className={styles.indicatorPlayerNumber}>Player {currentTurn}&apos;s Turn</div>
+                                <div className={styles.timeLeft}>{timeLeft}s</div>
+                            </div>
+                        </div>
+                    </div>
+    )
+}
--- src/views/Play.jsx
+++ src/views/Play.jsx
@@ -4,8 +4,9 @@ import player1 from '../assets/images/you.svg';
 import player2 from '../assets/images/cpu.svg';
 import gridFront from '../assets/images/grid-front-layer.svg';
 import gridBack from '../assets/images/grid-back-layer.svg';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import Disk from '../components/Disk';
+import PlayerIndicator from '../components/PlayerIndicator';
 
 export default function Play() {
     let [player1Score, setPlayer1Score] = useState(0);
@@ -14,6 +15,7 @@ export default function Play() {
     let [placedDisks, setPlacedDisks] = useState(new Array(6).fill(new Array(7).fill(0)))
     let [winner, setWinner] = useState(0);
     const gridRef = useRef(null)
+
 
     function handleGridClick(event) {
         let boundingRect = event.target.getBoundingClientRect()
@@ -47,13 +49,31 @@ export default function Play() {
             }
         })
         setPlacedDisks(newPlacedDisks)
-        checkWin({x: cellX, y: finalCellY}, currentTurn, newPlacedDisks)
+        let win = checkWin({x: cellX, y: finalCellY}, currentTurn, newPlacedDisks)
+
+        if (win) {
+            handleWin()
+        }
     }
+
 
     function restart() {
         setPlacedDisks(new Array(6).fill(new Array(7).fill(0)))
         setCurrentTurn(1)
     }
+
+    
+    function handleWin() {
+        if (currentTurn == 1) {
+            setPlayer1Score(player1Score + 1)
+        } else if (currentTurn == 2) {
+            setPlayer2Score(player2Score + 1)
+        }
+
+        setWinner(currentTurn)
+
+    }
+
 
     function checkWin(lastDiskPosition, lastPlayer, nextPlacedDisksState) {
         const {x, y} = lastDiskPosition
@@ -63,7 +83,6 @@ export default function Play() {
             if (nextPlacedDisksState[y][i] == lastPlayer) {
                 consecutive++
                 if (consecutive == 4) {
-                    console.log('horizontal win')
                     return true
                 }
             } else {
@@ -77,7 +96,6 @@ export default function Play() {
             if (nextPlacedDisksState[j][x] == lastPlayer) {
                 consecutive++
                 if (consecutive == 4) {
-                    console.log('vertical win')
                     return true
                 }
             } else {
@@ -98,7 +116,6 @@ export default function Play() {
             if (nextPlacedDisksState[workingY][workingX] == lastPlayer) {
                 consecutive++
                 if (consecutive == 4) {
-                    console.log('main diagonal win')
                     return true
                 }
             } else {
@@ -121,7 +138,6 @@ export default function Play() {
             if (nextPlacedDisksState[workingY][workingX] == lastPlayer) {
                 consecutive++
                 if (consecutive == 4) {
-                    console.log('anti diagonal win')
                     return true
                 }
             } else {
@@ -132,6 +148,7 @@ export default function Play() {
         }
         return false
     }
+
 
     function createDiskElements() {
         let diskElements = []
@@ -193,30 +210,7 @@ export default function Play() {
                     </div>
                 </div>
                 {!winner &&
-                    <div className={styles.indicatorSection}>
-                        <div className={styles.indicator}>
-                            <svg className={styles.indicatorImage} width="197" height="165" viewBox="0 0 197 165" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g filter="url(#filter0_d_5_3228)">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3 55.2795C3 47.2326 7.82258 39.9694 15.2389 36.8468L90.2793 5.25082C95.2186 3.17114 100.786 3.16075 105.733 5.22198L181.692 36.8718C189.145 39.9772 194 47.2593 194 55.3333V132C194 143.046 185.046 152 174 152H23C11.9543 152 3 143.046 3 132V55.2795Z" fill="#FD6687" />
-                                    <path d="M14.6568 35.4643C6.68427 38.8212 1.5 46.6291 1.5 55.2795V132C1.5 143.874 11.1259 153.5 23 153.5H174C185.874 153.5 195.5 143.874 195.5 132V55.3333C195.5 46.6538 190.281 38.8255 182.269 35.4872L106.31 3.83737C100.992 1.62154 95.0069 1.63271 89.6972 3.86836L14.6568 35.4643Z" stroke="black" stroke-width="3" />
-                                </g>
-                                <defs>
-                                    <filter id="filter0_d_5_3228" x="0" y="0.683594" width="197" height="164.316" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                                        <feOffset dy="10" />
-                                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0" />
-                                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_5_3228" />
-                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_5_3228" result="shape" />
-                                    </filter>
-                                </defs>
-                            </svg>
-                            <div className={styles.indicatorText}>
-                                <div className={styles.indicatorPlayerNumber}>Player {currentTurn}'s Turn</div>
-                                <div className={styles.timeLeft}>10s</div>
-                            </div>
-                        </div>
-                    </div>
+                    <PlayerIndicator currentTurn={currentTurn} setCurrentTurn={setCurrentTurn}/>
                 }
                 {winner !== 0 && <p>Winner!</p>}
             </main>
